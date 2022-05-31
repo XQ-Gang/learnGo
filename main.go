c505649a9fc95e20430dd05ac8f6f6ea8d81f6a4
XQ-Gang <3035686788@qq.com>
2022-05-31 14:48:19 +0800

    读书笔记：Go 语言精进之路 10-使用 iota 实现枚举常量

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import (
-	"github.com/XQ-Gang/learnGo/standard"
+const (
+	a, b = 1, 11
+	c, d
+	e, f
+)
+
+const g = iota
+const h = iota
+const i = 4
+const j = iota
+
+const (
+	_     = iota
+	Blue  // 1
+	Black // 2
+	Red   // 3
+	_
+	Yellow = iota * 2 // 10
+	Green             // 12
 )
 
 func main() {
@@ -9,5 +26,9 @@ func main() {
 	// redis.LearnRedis()
 
 	// learn standard library
-	standard.LearnStandard()
+	//standard.LearnStandard()
+
+	const k = iota
+	println(a, b, c, d, e, f)
+	println(g, h, i, j, k)
 }
