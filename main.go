59bb1c1a9973f622dfea9d312d0f605e6b947533
XQ-Gang <3035686788@qq.com>
2022-05-31 15:17:12 +0800

    读书笔记：Go 语言精进之路 11-尽量定义零值可用的类型

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 const (
 	a, b = 1, 11
 	c, d
@@ -31,4 +33,26 @@ func main() {
 	const k = iota
 	println(a, b, c, d, e, f)
 	println(g, h, i, j, k)
+
+	// 零值可用的切片不能通过下标形式操作数据
+	var s []int
+	s[0] = 12         // 报错！
+	s = append(s, 12) // 正确
+
+	// map 没有提供零值可用支持
+	var m map[string]int
+	m["go"] = 1 // 报错！
+
+	m1 := make(map[string]int)
+	m1["go"] = 1 // 正确
+
+	// 尽量避免值复制
+	var mu sync.Mutex
+	mu1 := mu
+	foo(mu)
+
+	// 可以通过指针方式传递类似 Mutex 这样的类型
+	var mu sync.Mutex
+	foo(&mu)
+
 }
