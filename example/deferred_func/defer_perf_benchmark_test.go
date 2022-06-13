d574f4e5090dc444afe74c72ab09dbd16bab5b8a
XQ-Gang <3035686788@qq.com>
2022-06-13 23:54:49 +0800

    读书笔记：Go 语言精进之路 22-使用 defer 让函数更简洁、更健壮2

---
+++ example/deferred_func/defer_perf_benchmark_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func sum(max int) int {
+	total := 0
+	for i := 0; i < max; i++ {
+		total += i
+	}
+
+	return total
+}
+
+func fooWithDefer() {
+	defer func() {
+		sum(10)
+	}()
+}
+
+func fooWithoutDefer() {
+	sum(10)
+}
+
+func BenchmarkFooWithDefer(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fooWithDefer()
+	}
+}
+
+func BenchmarkFooWithoutDefer(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fooWithoutDefer()
+	}
+}
