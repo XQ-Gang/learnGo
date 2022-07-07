071b080667f1605663ab5e613f89f6b631843062
XQ-Gang <3035686788@qq.com>
2022-07-07 23:23:24 +0800

    读书笔记：Go 语言精进之路 37-了解错误处理的 4 种策略

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"net"
+)
+
+type MyError struct {
+	e string
+}
+
+func (e *MyError) Error() string {
+	return e.e
+}
+
 func main() {
-	s := "world"
-	for _, v := range []byte(s) {
-		_ = v
+	var err = &MyError{"my error type"}
+	err1 := fmt.Errorf("wrap err1: %w", err)
+	err2 := fmt.Errorf("wrap err2: %w", err1)
+	var e *MyError
+	if errors.As(err2, &e) {
+		println("err is a variable of MyError type ")
+		println(e == err)
+		return
 	}
+
+	println("err is not a variable of the MyError type ")
+	net.Error()
 }
