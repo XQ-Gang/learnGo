55359b5c58fbc337fdaba39c3b423c5c3666b56a
XQ-Gang <3035686788@qq.com>
2022-08-28 22:31:13 +0800

    面试题: 判断两个给定的字符串排序后是否一致

--- interview/3-判断字符串中字符是否全都不同.go
+++ interview/3-判断字符串中字符是否全都不同.go
@@ -3,15 +3,11 @@ package main
 // 判断字符串中字符是否全都不同
 // 请实现一个算法，确定一个字符串的所有字符【是否全都不同】。这里我们要求【不允许使用额外的存储结构】。
 // 给定一个string，请返回一个bool值，true代表所有字符全都不同，false代表存在相同的字符。
-// 保证字符串中的字符为【ASCII字符】。字符串的长度小于等于【3000】。
+// 保证字符串中的字符为【ASCII字符】。
 
 // 思路：使用位运算来判断是否重复
 
-func isUniqString(s string) bool {
-	if len(s) == 0 || len(s) > 3000 {
-		return false
-	}
-	// 256 个字符 256 = 64 + 64 + 64 + 64
+func isUniqString(s string) bool { // 256 个字符 256 = 64 + 64 + 64 + 64
 	var mark1, mark2, mark3, mark4 uint64
 	var mark *uint64
 	for _, r := range s {
--- interview/4-判断两个给定的字符串排序后是否一致.go
+++ interview/4-判断两个给定的字符串排序后是否一致.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"strings"
+)
+
+// 判断两个给定的字符串排序后是否一致
+// 给定两个字符串，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+// 给定一个string s1和一个string s2，请返回一个bool，代表两串是否重新排列后可相同。
+
+func isRegroup(s1, s2 string) bool {
+	for _, v := range s1 {
+		if strings.Count(s1, string(v)) != strings.Count(s2, string(v)) {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	s1 := "abcdefg"
+	s2 := "gfedcba"
+	println(isRegroup(s1, s2))
+}
