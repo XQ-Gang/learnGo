6eb759a6b7e43cda447f8528949c262c22e42ae4
XQ-Gang <3035686788@qq.com>
2023-07-13 14:55:54 +0800

    feat: bounds checking elimination

---
+++ example/bounds_checking_elimination.go
@@ -0,0 +1,22 @@
+package main
+
+func normal(s []int) {
+	i := 0
+	i += s[0]
+	i += s[1]
+	i += s[2]
+	i += s[3]
+	println(i)
+}
+
+// 如果能确定访问到的  slice 长度，可以先执行一次让编译器去做优化
+// [Bounds Checking Elimination](https://docs.google.com/document/d/1vdAEAjYdzjnPA9WDOQ1e4e05cYVMpqSxJYZT33Cqw2g/edit)
+func bce(s []int) {
+	_ = s[3]
+	i := 0
+	i += s[0]
+	i += s[1]
+	i += s[2]
+	i += s[3]
+	println(i)
+}
