b1a647b573ede190c51594ce400b23a53da348aa
XQ-Gang <3035686788@qq.com>
2023-07-18 13:56:57 +0800

    feat: interface assertion

--- example/interface/interface_assertion.go
+++ example/interface/interface_assertion.go
@@ -4,12 +4,28 @@ import (
 	"fmt"
 )
 
-func getNum() any {
+func getInteger() any {
 	return 100
 }
 
+func getFloat() any {
+	return 1.1
+}
+
 func main() {
-	res, ok := getNum().(int64)
-	fmt.Println(res) // 0
-	fmt.Println(ok)  // false
+	res1, ok := getInteger().(int64)
+	fmt.Println(res1) // 0
+	fmt.Println(ok)   // false
+
+	res2, ok := getInteger().(int)
+	fmt.Println(res2) // 100
+	fmt.Println(ok)   // true
+
+	res3, ok := getFloat().(float32)
+	fmt.Println(res3) // 0
+	fmt.Println(ok)   // false
+
+	res4, ok := getFloat().(float64)
+	fmt.Println(res4) // 1.1
+	fmt.Println(ok)   // true
 }
