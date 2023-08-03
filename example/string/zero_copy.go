6dc2741f604f27d9807f8b7915950f22a04db5bf
XQ-Gang <3035686788@qq.com>
2023-08-04 01:29:14 +0800

    feat: zero copy

--- example/string/zero_copy.go
+++ example/string/zero_copy.go
@@ -7,32 +7,29 @@ import (
 
 // StringToBytes supports since Go1.20+.
 func StringToBytes(s string) []byte {
+	if s == "" {
+		return nil
+	}
 	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
 
 // BytesToString supports since Go1.20+.
 func BytesToString(b []byte) string {
-	return unsafe.String(&b[0], len(b))
+	if len(b) == 0 {
+		return ""
+	}
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
 
 func main() {
 	s := "ab"
-	fmt.Printf("%p\n", &s)
+	fmt.Printf("%p\n", &s) // 0xc000014270
 	b := StringToBytes(s)
-	fmt.Printf("%p\n", &b)
+	fmt.Printf("%p\n", &b) // 0xc000010030
 	s2 := BytesToString(b)
-	fmt.Printf("%p\n", &s2)
-	fmt.Println(b)
-	fmt.Println(string(b))
-	fmt.Println(s2)
-	fmt.Println([]byte(s2))
-
-	// Output:
-	// 0xc000014270
-	// 0xc000010030
-	// 0xc000014280
-	// [97 98]
-	// ab
-	// ab
-	// [97 98]
+	fmt.Printf("%p\n", &s2) // 0xc000014280
+	fmt.Println(b)          // [97 98]
+	fmt.Println(string(b))  // ab
+	fmt.Println(s2)         // ab
+	fmt.Println([]byte(s2)) // [97 98]
 }
